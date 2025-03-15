4532f5b0b1884409448d27756cb49f5313439f95
Fabio Dijkshoorn <offabio@outlook.com>
2025-03-15 14:52:49 +0100

    feat: added maven packaging

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    kotlin("jvm") version "1.8.22"
+    `maven-publish`
+}
+
+group = "net.nouw"
+version = "1.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+sourceSets {
+    main {
+        kotlin {
+            srcDir("generated/kotlin")
+        }
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    // Add your other dependencies here
+}
+
+// Configure publishing to GitHub Packages
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])  // Exposes the compiled JAR and accompanying metadata
+            groupId = "net.nouw"
+            artifactId = "convergence-of-crowns-model"
+            version = "1.0.1"
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            // Change the URL if needed. For a personal account, use your username.
+            url = uri("https://maven.pkg.github.com/nouw/convergence-of-crowns-model")
+            credentials {
+                // Preferably, use project properties if set, or fall back to environment variables.
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("PACKAGE_ACTOR")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("NPM_TOKEN")
+            }
+        }
+    }
+}
