959da6f7d7da3427a009c697bbfd2f120507087d
Fabio Dijkshoorn <offabio@outlook.com>
2025-03-15 17:33:16 +0100

    fix: publishing to repo instead of profile

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "net.nouw"
-version = "1.0.3"
+version = "1.0.4"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,7 @@ publishing {
             from(components["java"])  // Exposes the compiled JAR and accompanying metadata
             groupId = "net.nouw"
             artifactId = "convergence-of-crowns-model"
-            version = "1.0.3"
+            version = "1.0.4"
         }
     }
     repositories {
